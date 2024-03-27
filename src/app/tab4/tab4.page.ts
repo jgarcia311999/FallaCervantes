e37c9d88ef92655fbd45b8b8b507ad20be29c121
Jesús García Alemany <jyjmatet@gmail.com>
2024-03-27 21:23:25 +0100

    funciona la fcking apiiiiiii

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -9,21 +9,44 @@ export class Tab4Page  {
 
   constructor() { }
 
-  photos: string[] = [
-    'assets/images/fallaGrande1.jpg',
-    'assets/images/fallaGrande2.jpg',
-    'assets/images/fallaGrande3.jpg',
-    'assets/images/fallaGrande4.jpg',
-    'assets/images/fallaGrande5.jpg',
-    'assets/images/fallaGrande6.jpg',
-    'assets/images/fallaGrande7.jpg',
-    'assets/images/fallaGrande8.jpg',
-    'assets/images/fallaGrande9.jpg'
-    ];
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit() {
+    this.fetchImages();
+  }
 
-  primeraLinea: string = 'Som '
-  primeraPalabra: string = 'xics i xiques'
-  segundaLinea: string = 'vine al nostre  '
-  segundaPalabra: string = 'casal'
 
+  photos: string[] = [];
+
+
+
+  primeraLinea: string = 'a ferse un par de '
+  primeraPalabra: string = 'cubates'
+  segundaLinea: string = 'la son  '
+  segundaPalabra: string = 'te anira'
+
+
+  token: string = "IGQWRQU3VBdkt1OU9sZAkZAyQ0Rob1lid1VKSlM4b0NIZA2t6VXp2enJVTXdZAdS1RM29sNUJsMkZA6Y3lFQlI1X3JIUFpEUzc5eXhmeUpVaTEyTVU1NjBQNXBmWEpkVWlrVERHekFFRXZAtXzhyc09MeGxXbHZAId3dyUlkZD"
+  fetchImages() {
+    const url = `https://graph.instagram.com/me/media?fields=id,media_url&access_token=${this.token}`;
+    console.log("URL de solicitud:", url); // Agregar mensaje de consola para la URL de solicitud
+    fetch(url)
+      .then(res => res.json())
+      .then((data: any) => {
+        console.log("Datos recibidos de Instagram:", data); // Agregar mensaje de consola para los datos recibidos
+        if (data.data) {
+          this.createPhotos(data.data);
+        } else {
+          console.error("Error fetching images:", data);
+        }
+      })
+      .catch(error => console.error("Error fetching images:", error));
+  }
+
+  createPhotos(data: any[]) {
+    this.photos = [];
+    for (const img of data) {
+      this.photos.push(img.media_url);
+    }
+    console.log("Fotos creadas:", this.photos); // Agregar mensaje de consola para las fotos creadas
+  }
 }
---
+++ src/app/tab5/tab5.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab5',
+  templateUrl: './tab5.page.html',
+  styleUrls: ['./tab5.page.scss'],
+})
+export class Tab5Page  {
+
+  constructor() { }
+
+  primeraLinea: string = 'parapa '
+  primeraPalabra: string = 'pa pa'
+  segundaLinea: string = 'parapa  '
+  segundaPalabra: string = 'parapa'
+ 
+}
