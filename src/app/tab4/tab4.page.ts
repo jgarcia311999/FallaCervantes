fdbb3badc841780e4cdd0875e2aa88c7ac426952
JesusGarciaAlemany31 <jgarcia@icloudcompliance.com>
2024-04-24 12:58:36 +0200

    loader para la galeria de instagram

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -9,32 +9,24 @@ export class Tab4Page  {
 
   constructor() { }
 
-  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  isLoading: boolean = true; // Variable para controlar la visibilidad del loader
+  photos: string[] = [];
+  token: string = "IGQWRQU3VBdkt1OU9sZAkZAyQ0Rob1lid1VKSlM4b0NIZA2t6VXp2enJVTXdZAdS1RM29sNUJsMkZA6Y3lFQlI1X3JIUFpEUzc5eXhmeUpVaTEyTVU1NjBQNXBmWEpkVWlrVERHekFFRXZAtXzhyc09MeGxXbHZAId3dyUlkZD";
+
   ngOnInit() {
     this.fetchImages();
   }
 
-
-  photos: string[] = [];
-
-
-
-  primeraLinea: string = 'a ferse un par de '
-  primeraPalabra: string = 'cubates'
-  segundaLinea: string = 'la son  '
-  segundaPalabra: string = 'te anira'
-
-
-  token: string = "IGQWRQU3VBdkt1OU9sZAkZAyQ0Rob1lid1VKSlM4b0NIZA2t6VXp2enJVTXdZAdS1RM29sNUJsMkZA6Y3lFQlI1X3JIUFpEUzc5eXhmeUpVaTEyTVU1NjBQNXBmWEpkVWlrVERHekFFRXZAtXzhyc09MeGxXbHZAId3dyUlkZD"
   fetchImages() {
     const url = `https://graph.instagram.com/me/media?fields=id,media_url&access_token=${this.token}`;
-    console.log("URL de solicitud:", url); // Agregar mensaje de consola para la URL de solicitud
+    console.log("URL de solicitud:", url);
     fetch(url)
       .then(res => res.json())
       .then((data: any) => {
-        console.log("Datos recibidos de Instagram:", data); // Agregar mensaje de consola para los datos recibidos
+        console.log("Datos recibidos de Instagram:", data);
         if (data.data) {
           this.createPhotos(data.data);
+          this.isLoading = false; // Ocultar el loader cuando se hayan cargado las fotos
         } else {
           console.error("Error fetching images:", data);
         }
@@ -47,6 +39,6 @@ export class Tab4Page  {
     for (const img of data) {
       this.photos.push(img.media_url);
     }
-    console.log("Fotos creadas:", this.photos); // Agregar mensaje de consola para las fotos creadas
+    console.log("Fotos creadas:", this.photos);
   }
 }
