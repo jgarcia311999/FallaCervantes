8ef078efed911c67df2c8902f3f52a1ebfac39fb
JesusGarciaAlemany31 <jgarcia@icloudcompliance.com>
2024-04-24 16:49:46 +0200

    cambios

--- src/app/event-form/event-form.page.ts
+++ src/app/event-form/event-form.page.ts
@@ -14,10 +14,48 @@ export class EventFormPage {
   constructor() {}
 
   guardarEvento() {
-    // Aquí puedes guardar los datos del evento, por ejemplo, enviarlos a un servicio
-    console.log('Nombre del Evento:', this.nombreEvento);
-    console.log('Descripción:', this.descripcion);
-    console.log('Fecha del Evento:', this.fechaEvento);
+    // Verificar si this.fechaEvento está definido y no es null
+    if (!this.fechaEvento) {
+      console.error('Error: La fecha no está definida');
+      return;
+    }
+  
+    // Convertir this.fechaEvento a un objeto Date
+    const fecha = new Date(this.fechaEvento);
+  
+    // Verificar si la conversión fue exitosa
+    if (isNaN(fecha.getTime())) {
+      console.error('Error: La fecha no es válida');
+      return;
+    }
+  
+    // Obtener la fecha en formato YYYY-MM-DD
+    const fechaFormateada = fecha.toISOString().slice(0, 10);
+  
+    // Crear un objeto con los datos del evento
+    const nuevoEvento = {
+      date: fechaFormateada,
+      textColor: 'rgb(251, 165, 35)',
+      backgroundColor: 'rgb(251, 165, 35)',
+      eventos: [
+        {
+          titulo: this.nombreEvento,
+          descripcion: this.descripcion
+        }
+      ]
+    };
+  
+    // Agregar el nuevo evento a la lista de eventos existente o crear una nueva lista si aún no existe
+    /* if (!this.listaEventos) {
+      this.listaEventos = [nuevoEvento];
+    } else {
+      this.listaEventos.push(nuevoEvento);
+    } */
+  
+    // Mostrar los datos guardados
+    console.log('Evento guardado:', nuevoEvento);
   }
+  
+  
 
 }
