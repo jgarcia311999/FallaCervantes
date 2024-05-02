3f943059ef68527b5459a286f936f011b3bc58f0
JesusGarciaAlemany31 <jgarcia@icloudcompliance.com>
2024-05-02 15:26:05 +0200

    correo mostrado en el perfil

--- src/app/servicios/user.service.ts
+++ src/app/servicios/user.service.ts
@@ -8,20 +8,23 @@ export class UserService {
 
   constructor(private auth: Auth) { }
 
+  // Función para registrar un nuevo usuario con correo electrónico y contraseña
   register(email: string, password: string) {
     return createUserWithEmailAndPassword(this.auth, email, password);
   }
 
+  // Función para iniciar sesión con correo electrónico y contraseña
   login(email: string, password: string) {
     return signInWithEmailAndPassword(this.auth, email, password);
   }
 
+  // Función para cerrar sesión
   logout() {
     return signOut(this.auth);
   }
 
+  // Función para obtener el usuario actualmente autenticado
   getCurrentUser(): User | null {
     return this.auth.currentUser;
   }
-
 }
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../servicios/user.service';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
@@ -8,11 +8,19 @@ import { ModalController } from '@ionic/angular';
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
 })
-export class Tab5Page {
+export class Tab5Page implements OnInit {
 
-  selectedImage: string = '../../assets/images/userIconB.webp'; 
+  selectedImage: string = '../../assets/images/placeholder.PNG'; 
+  correoUsuario: string | null = null; // Variable para almacenar el correo del usuario
 
   constructor(private userService: UserService, private router: Router, private modalController: ModalController) { }
+  ngOnInit(): void {
+    // Al iniciar la página, obtener el usuario actual y su correo electrónico
+    const usuarioActual = this.userService.getCurrentUser();
+    if (usuarioActual) {
+      this.correoUsuario = usuarioActual.email;
+    }
+  }
 
   onClick() {
     this.userService.logout()
