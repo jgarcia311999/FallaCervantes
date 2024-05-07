590bcbafe49ad081877bd632cab8d1f65f621f4e
JesusGarciaAlemany31 <jgarcia@icloudcompliance.com>
2024-05-07 13:48:32 +0200

    cambios en la vista de ordenador

--- src/app/info-page/info-page.page.ts
+++ src/app/info-page/info-page.page.ts
@@ -9,10 +9,23 @@ import { ActivatedRoute } from '@angular/router';
 export class InfoPagePage implements OnInit {
   itemId: number | undefined;
   items: any[] = [
-    { id: 1, title: 'La nostra fallera major', text: 'Coneix a la nostra fallera major, la representant principal de la nostra comissió fallera. Descobreix la seua història, els seus èxits i el seu compromís amb les nostres tradicions.' },
-    { id: 2, title: 'El nostre president', text: 'Descobreix qui lidera la nostra comissió fallera com a president. Coneix els seus objectius, la seua visió de futur i com treballa per fer realitat les il·lusions de tots nosaltres.' },
-    { id: 3, title: 'La nostra fallera major infantil', text: 'Coneix a la nostra fallera major infantil, la jove que representa l alegria i l energia de la nostra comissió fallera. Descobreix les seues activitats, la seua creativitat i com contagia el seu entusiasme.' },
-    { id: 4, title: 'El nostre president infantil', text: 'Coneix al líder més jove de la nostra comissió fallera, el nostre president infantil. Descobreix com inspira als més menuts, el seu paper en les nostres activitats i com porta endavant la diversió i la camaraderia.' }
+    { 
+      id: 1, 
+      title: 'La nostra fallera major', 
+      text: 'Coneix a la nostra fallera major, la representant principal de la nostra comissió fallera. Descobreix la seua història, els seus èxits i el seu compromís amb les nostres tradicions.' 
+    },
+    { id: 2, 
+      title: 'El nostre president', 
+      text: 'Descobreix qui lidera la nostra comissió fallera com a president. Coneix els seus objectius, la seua visió de futur i com treballa per fer realitat les il·lusions de tots nosaltres.' 
+    },
+    { id: 3, 
+      title: 'La nostra fallera major infantil', 
+      text: 'Coneix a la nostra fallera major infantil, la jove que representa l alegria i l energia de la nostra comissió fallera. Descobreix les seues activitats, la seua creativitat i com contagia el seu entusiasme.' 
+    },
+    { id: 4, 
+      title: 'El nostre president infantil', 
+      text: 'Coneix al líder més jove de la nostra comissió fallera, el nostre president infantil. Descobreix com inspira als més menuts, el seu paper en les nostres activitats i com porta endavant la diversió i la camaraderia.' 
+    }
 
   ];
 
